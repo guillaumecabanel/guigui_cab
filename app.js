884676949491f65d3e3915899b0a31db7f80ad5f
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2020-03-01 01:03:23 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const sendAnalytics = (dimension, value) => {
+  const query = {
+    name: "web.guigui.${dimension}",
+    value: 1,
+    dimension: value
+  }
+
+  fetch(baseUri, {
+    method: "POST",
+    headers: {
+      'x-qm-key': quickMetricsKey,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query: query })
+  })
+}
+
+const baseUri = 'https://qckm.io/json';
+const quickMetricsKey ='xRzQjjvlAX0iXJ2_NiN6OA';
+
+sendAnalytics('ua', navigator.userAgent);
+sendAnalytics('platform', navigator.platform);
+sendAnalytics('language', navigator.language);
